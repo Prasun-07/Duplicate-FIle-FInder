ade527c8e5167883d6f952b0e0e149034a1915ba
Prasun-07 <biswasprasun67@gmail.com>
2025-05-18 21:23:29 +0530

    Add initial dialog box layout

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
+
+        SwingUtilities.invokeLater(MainFrame::new);
     }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,13 +1,50 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class MainFrame extends JFrame {
-    public MainFrame() {
-        setTitle("Duplicate File Finder");
-        setSize(600, 400);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null); // center window
 
-        JLabel label = new JLabel("Welcome to Duplicate File Finder");
-        add(label);
+    private JLabel selectedPathLabel;
+    public MainFrame() {
+        //setting the layout
+        setTitle("Duplicate FIle Finder");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);                            //we set the default operation onclcicking close
+        setSize(500, 500);
+        setLayout(new BorderLayout());                                      //borderlayout function (start(NORTH), end(SOUTH), left, right, center)
+
+        //label to show path at the top of the window
+        JLabel titleLabel = new JLabel("Select the file");              //creates the label
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);           //aligns the label in the center
+        add(titleLabel, BorderLayout.NORTH);                                //we place this label to the top(north) section of the window
+
+        //shows the selected folder path (initially no path selected)
+        selectedPathLabel= new JLabel("No File Selected");
+        selectedPathLabel.setHorizontalAlignment((SwingConstants.CENTER));
+        add(selectedPathLabel, BorderLayout.CENTER);                        //we place this label to the top(north) section of the window
+
+        //browsing button adding
+        JButton browseButton = new JButton("Browse Files");
+        browseButton.addActionListener(this::onBrowseClicked);              //when button is clicked, it will render the onBrowseCLicked() function
+        add(browseButton, BorderLayout.SOUTH);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+
+    }
+
+    //function for when browse button is clicked
+    public void onBrowseClicked(ActionEvent e){
+        JFileChooser chooser = new JFileChooser();                      //navigate through files ( dialog box)
+        chooser.setDialogTitle("Select A Folder");
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);    //sets the file selection to be able to choose only directories
+        chooser.setAcceptAllFileFilterUsed(false);                      //blocks from selecting all files
+
+        int chosenOption = chooser.showOpenDialog(this);
+        if(chosenOption == JFileChooser.APPROVE_OPTION){
+            File chosenFolder = chooser.getSelectedFile();
+            selectedPathLabel.setText("Selected: "+chosenFolder.getAbsolutePath());
+        }
     }
 }
